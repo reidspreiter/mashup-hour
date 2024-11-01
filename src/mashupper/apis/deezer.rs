3914341257e2907c10fdedfaabc74ac0047391a1
reidspreiter <reidspreiter@gmail.com>
2024-10-31 22:13:28 -0500

    src/mashupper/apis/deezer: add pagination and find_track endpoint

--- src/mashupper/apis/deezer.rs
+++ src/mashupper/apis/deezer.rs
@@ -1,12 +1,29 @@
-use super::base::APIResult;
-use crate::{Client, Error, Result};
-use serde::Deserialize;
+use super::base::{request, APIResult, Pagination};
+use crate::Result;
+use serde::{de::DeserializeOwned, Deserialize};
 
 #[derive(Debug, Deserialize)]
-pub struct DeezerResponse<T> {
+pub struct DeezerPaginationResponse<T> {
     pub data: T,
     pub next: String,
     pub total: u64,
+}
+
+impl<T> Pagination for DeezerPaginationResponse<T>
+where
+    T: DeserializeOwned,
+{
+    fn get_pagination_url(&self, url: &str, page_index: &u64) -> String {
+        format!("{}&index={}", url, page_index)
+    }
+
+    fn page_limit(&self) -> u64 {
+        25
+    }
+
+    fn next(&self) -> &String {
+        &self.next
+    }
 }
 
 pub type TrackList = Vec<SearchTrack>;
@@ -14,13 +31,9 @@ pub type TrackList = Vec<SearchTrack>;
 #[derive(Debug, Deserialize)]
 pub struct SearchTrack {
     pub id: u64,
-    pub title: String,
-    pub album: Album,
-    pub artist: Artist,
-    #[serde(rename = "link")]
-    pub track_url: String,
     #[serde(rename = "preview")]
     pub preview_url: String,
+    // More fields exist in the response json
 }
 
 #[derive(Debug, Deserialize)]
@@ -28,27 +41,35 @@ pub struct Album {
     pub title: String,
     #[serde(rename = "cover_big")]
     pub cover_url: String,
+    // More fields exist in the response json
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Artist {
     pub name: String,
+    // More fields exist in the response json
 }
 
-pub async fn search_tracks(
-    client: &Client,
-    query: &str,
-) -> Result<APIResult<DeezerResponse<TrackList>>> {
-    let url = format!("https://api.deezer.com/search/track?q={}", query);
-    let res = client.get(&url).send().await?;
+#[derive(Debug, Deserialize)]
+pub struct Track {
+    pub id: u64,
+    pub title: String,
+    pub title_short: String,
+    pub bpm: f32,
+    pub gain: f32,
+    #[serde(rename = "preview")]
+    pub preview_url: String,
+    pub artist: Artist,
+    pub album: Album,
+    // More fields exist in the response json
+}
 
-    if res.status().is_success() {
-        let text = res.text().await?;
-        let response: DeezerResponse<TrackList> = serde_json::from_str(&text)?;
-        return Ok(APIResult { url, response });
-    }
-    Err(Error::ResponseError {
-        status_code: res.status().as_u16(),
-        message: res.text().await?,
-    })
+pub async fn search_tracks(query: &str) -> Result<APIResult<DeezerPaginationResponse<TrackList>>> {
+    let url = format!("https://api.deezer.com/search/track?q={}", query);
+    request::<DeezerPaginationResponse<TrackList>>(&url).await
+}
+
+pub async fn find_track(track_id: &u64) -> Result<APIResult<Track>> {
+    let url = format!("https://api.deezer.com/track/{}", track_id);
+    request::<Track>(&url).await
 }
