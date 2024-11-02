9216024fe88797e60bd34213a408e911dc1bddc8
reidspreiter <reidspreiter@gmail.com>
2024-11-02 11:07:04 -0500

    src/mashupper/apis/base: add data-specific request functions
    Additionally, make next_page return an option to handle last pages
    exposing no next url.

--- src/mashupper/apis/base.rs
+++ src/mashupper/apis/base.rs
@@ -1,11 +1,11 @@
 use crate::{Error, Result};
-use reqwest::Client;
+use reqwest::{Client, Response};
 use serde::de::DeserializeOwned;
 
 pub trait Pagination: DeserializeOwned {
     fn page_limit(&self) -> u64;
     fn get_pagination_url(&self, url: &str, page_index: &u64) -> String;
-    fn next(&self) -> &String;
+    fn next(&self) -> &Option<String>;
 }
 
 pub struct APIResult<T> {
@@ -20,32 +20,47 @@ where
     pub async fn get_page_from_index(&self, entry_index: &u64) -> Result<APIResult<T>> {
         let page_index = entry_index / self.response.page_limit();
         let url = self.response.get_pagination_url(&self.url, &page_index);
-        request(&url).await
+        request_model(&url).await
     }
 
-    pub async fn next_page(&self) -> Result<APIResult<T>> {
-        let url = self.response.next();
-        request(&url).await
+    pub async fn next_page(&self) -> Result<Option<APIResult<T>>> {
+        match self.response.next() {
+            Some(url) => Ok(Some(request_model(&url).await?)),
+            None => Ok(None),
+        }
     }
 }
 
-pub async fn request<T>(url: &str) -> Result<APIResult<T>>
-where
-    T: DeserializeOwned,
-{
+async fn request(url: &str) -> Result<Response> {
     let client = Client::new();
     let res = client.get(url).send().await?;
-
     if res.status().is_success() {
-        let text = res.text().await?;
-        let response: T = serde_json::from_str(&text)?;
-        return Ok(APIResult {
-            url: url.to_string(),
-            response,
-        });
+        return Ok(res);
     }
     Err(Error::ResponseError {
         status_code: res.status().as_u16(),
         message: res.text().await?,
+    })
+}
+
+pub async fn request_model<T>(url: &str) -> Result<APIResult<T>>
+where
+    T: DeserializeOwned,
+{
+    let res = request(url).await?;
+    let text = res.text().await?;
+    let model: T = serde_json::from_str(&text)?;
+    Ok(APIResult {
+        url: url.to_string(),
+        response: model,
+    })
+}
+
+pub async fn request_bytes(url: &str) -> Result<APIResult<bytes::Bytes>> {
+    let res = request(url).await?;
+    let bytes = res.bytes().await?;
+    Ok(APIResult {
+        url: url.to_string(),
+        response: bytes,
     })
 }
