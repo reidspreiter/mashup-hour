5b6c6e25f7eec5e9ad60409feb31a9dad8f00f93
reidspreiter <reidspreiter@gmail.com>
2024-11-10 15:41:12 -0600

    src/main: add retrieve_assets endpoint

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,37 @@ pub use self::error::{Error, Result};
 use actix_files::Files;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result as ActixResult};
 use log::error;
+use redis::Client;
+use std::{env, sync::Arc};
 
-use assets::manager::refresh_assets;
+use assets::manager;
 
-#[get("/collect-mashup-assets")]
-async fn collect_mashup_assets() -> ActixResult<impl Responder> {
-    Ok(web::Json("Collected assets"))
+async fn get_redis_connection() -> Result<Client> {
+    let instance = env::var("UPSTASH_INSTANCE")?;
+    let key = env::var("UPSTASH_KEY")?;
+    let port = env::var("UPSTASH_PORT")?;
+    Ok(Client::open(format!(
+        "rediss://default:{key}@{instance}:{port}"
+    ))?)
 }
 
-#[post("/trigger_refresh")]
-async fn trigger_refresh() -> ActixResult<impl Responder> {
-    match refresh_assets().await {
+#[get("/retrieve-assets")]
+async fn retrieve_assets(redis_client: web::Data<Arc<Client>>) -> ActixResult<impl Responder> {
+    match manager::retrieve_assets(&redis_client).await {
+        Ok(assets) => Ok(HttpResponse::Ok().json(assets)),
+        Err(e) => {
+            error!("Error retrieving assets: {e}");
+            Ok(HttpResponse::InternalServerError().json("Encountered error retrieving assets"))
+        }
+    }
+}
+
+#[post("/refresh-assets")]
+async fn refresh_assets(redis_client: web::Data<Arc<Client>>) -> ActixResult<impl Responder> {
+    match manager::refresh_assets(&redis_client).await {
         Ok(_) => Ok(HttpResponse::Ok().json("Assets refreshed successfully")),
         Err(e) => {
-            error!("Error refreshing assets: {e:?}");
+            error!("Error refreshing assets: {e}");
             Ok(HttpResponse::InternalServerError().json("Encountered error refreshing assets"))
         }
     }
@@ -30,11 +47,13 @@ async fn trigger_refresh() -> ActixResult<impl Responder> {
 async fn main() -> std::io::Result<()> {
     env_logger::init();
     dotenv::dotenv().ok();
+    let redis_client = Arc::new(get_redis_connection().await?);
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
-            .service(collect_mashup_assets)
-            .service(trigger_refresh)
+            .app_data(web::Data::new(redis_client.clone()))
+            .service(retrieve_assets)
+            .service(refresh_assets)
             .service(Files::new("/", "./mashup-hour-frontend/dist").index_file("index.html"))
     })
     .bind(("127.0.0.1", 8080))?
