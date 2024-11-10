5d37633db7e3867c1b171f677476c1d7d98e4faa
reidspreiter <reidspreiter@gmail.com>
2024-11-10 15:31:11 -0600

    src/assets/manager: add redis set and get functionality

--- src/assets/manager.rs
+++ src/assets/manager.rs
@@ -3,7 +3,10 @@ use crate::{
     apis::supabase::{self as sb, MashupAssets},
     Result,
 };
+use actix_web::web::Data;
 use log::info;
+use redis::{aio::MultiplexedConnection, AsyncCommands, Client};
+use std::sync::Arc;
 
 async fn insert_new_asset_row() -> Result<()> {
     let track1 = build_track_asset().await?;
@@ -22,7 +25,7 @@ async fn insert_new_asset_row() -> Result<()> {
     Ok(())
 }
 
-fn get_delete_criteria(assets: &Vec<MashupAssets>) -> String {
+fn get_delete_criteria(assets: &Vec<sb::MashupAssets>) -> String {
     let mut criteria = String::from("id=not.in.(");
 
     for (i, mashup_assets) in assets.iter().enumerate() {
@@ -35,8 +38,7 @@ fn get_delete_criteria(assets: &Vec<MashupAssets>) -> String {
     criteria
 }
 
-pub async fn refresh_assets() -> Result<()> {
-    insert_new_asset_row().await?;
+async fn select_assets_from_database() -> Result<Vec<sb::MashupAssets>> {
     let assets = sb::select::<sb::MashupAssets>(
         "mashup_assets",
         Some("*"),
@@ -45,9 +47,45 @@ pub async fn refresh_assets() -> Result<()> {
     )
     .await?;
     info!("Selected: {:?}", assets.response);
+    Ok(assets.response)
+}
 
-    let delete_criteria = get_delete_criteria(&assets.response);
+async fn refresh_assets_cache(
+    assets: &Vec<sb::MashupAssets>,
+    connection: &mut MultiplexedConnection,
+) -> Result<()> {
+    let json = serde_json::to_string(&assets)?;
+    let _: () = connection.set("mashup_assets", &json).await?;
+    info!("Refreshed assets in cache");
+    Ok(())
+}
+
+pub async fn refresh_assets(client: &Data<Arc<Client>>) -> Result<()> {
+    insert_new_asset_row().await?;
+    let assets = select_assets_from_database().await?;
+
+    let mut conn = client.get_multiplexed_tokio_connection().await?;
+    refresh_assets_cache(&assets, &mut conn).await?;
+
+    let delete_criteria = get_delete_criteria(&assets);
     let deleted = sb::delete::<sb::MashupAssets>("mashup_assets", Some(&delete_criteria)).await?;
     info!("Deleted: {:?}", deleted.response);
     Ok(())
+}
+
+pub async fn retrieve_assets(client: &Data<Arc<Client>>) -> Result<Vec<MashupAssets>> {
+    let mut conn = client.get_multiplexed_tokio_connection().await?;
+    let cached_assets: Option<String> = conn.get("mashup_assets").await?;
+    match cached_assets {
+        Some(assets) => {
+            info!("Retrieved assets from cache");
+            Ok(serde_json::from_str::<Vec<MashupAssets>>(&assets)?)
+        }
+        None => {
+            info!("Cache miss");
+            let assets = select_assets_from_database().await?;
+            refresh_assets_cache(&assets, &mut conn).await?;
+            Ok(assets)
+        }
+    }
 }
