b4616d7071bb0f7ae505a8a40ba464e9068cf283
reidspreiter <reidspreiter@gmail.com>
2024-11-10 08:35:22 -0600

    src/apis/dictionary: update typing of optional fields

--- src/apis/dictionary.rs
+++ src/apis/dictionary.rs
@@ -1,29 +1,39 @@
 use super::base::{request, APIResult, RequestMethod};
 use crate::Result;
-use serde::Deserialize;
-
-pub type Words = Vec<Word>;
+use serde::{Deserialize, Serialize};
 
 const DICTIONARY_URL: &str = "https://api.dictionaryapi.dev/api/v2/";
 
-#[derive(Debug, Deserialize)]
+pub type Words = Vec<Word>;
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Word {
     pub word: String,
-    pub origin: String,
-    pub meanings: Vec<Meaning>,
+    pub origin: Option<String>,
+    pub meanings: Option<Vec<Meaning>>,
 }
 
-#[derive(Debug, Deserialize)]
+impl Word {
+    pub fn unknown(word: String) -> Self {
+        Self {
+            word,
+            origin: None,
+            meanings: None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Meaning {
     #[serde(rename = "partOfSpeech")]
     pub part_of_speech: String,
     pub definitions: Vec<Definition>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Definition {
     pub definition: String,
-    pub example: String,
+    pub example: Option<String>,
 }
 
 pub async fn search_dictionary(word: &str) -> Result<APIResult<Words>> {
