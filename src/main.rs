4b7109492bb56e7302de09dac0f91880a8d03eca
reidspreiter <reidspreiter@gmail.com>
2024-11-17 12:50:22 -0600

    src/main: add cors for development

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ mod error;
 
 pub use self::error::{Error, Result};
 
+use actix_cors::Cors;
 use actix_files::Files;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result as ActixResult};
-use log::error;
+use actix_web::{
+    get, http::header, post, web, App, HttpResponse, HttpServer, Responder, Result as ActixResult,
+};
+use log::{error, info};
 use redis::Client;
 use std::{env, sync::Arc};
 
@@ -23,6 +26,7 @@ async fn get_redis_connection() -> Result<Client> {
 
 #[get("/retrieve-assets")]
 async fn retrieve_assets(redis_client: web::Data<Arc<Client>>) -> ActixResult<impl Responder> {
+    info!("retrieving assets...");
     match manager::retrieve_assets(&redis_client).await {
         Ok(assets) => Ok(HttpResponse::Ok().json(assets)),
         Err(e) => {
@@ -34,6 +38,7 @@ async fn retrieve_assets(redis_client: web::Data<Arc<Client>>) -> ActixResult<im
 
 #[post("/refresh-assets")]
 async fn refresh_assets(redis_client: web::Data<Arc<Client>>) -> ActixResult<impl Responder> {
+    info!("refreshing assets...");
     match manager::refresh_assets(&redis_client).await {
         Ok(_) => Ok(HttpResponse::Ok().json("Assets refreshed successfully")),
         Err(e) => {
@@ -45,12 +50,21 @@ async fn refresh_assets(redis_client: web::Data<Arc<Client>>) -> ActixResult<imp
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::Info)
+        .init();
     dotenv::dotenv().ok();
     let redis_client = Arc::new(get_redis_connection().await?);
 
     HttpServer::new(move || {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:5173")
+                    .allowed_methods(vec!["GET"])
+                    .allowed_headers(vec![header::CONTENT_TYPE, header::AUTHORIZATION])
+                    .max_age(3600),
+            )
             .app_data(web::Data::new(redis_client.clone()))
             .service(retrieve_assets)
             .service(refresh_assets)
