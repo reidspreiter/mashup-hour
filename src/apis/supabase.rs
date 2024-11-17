52868eac596924371ea8afa23a6c9268d96a0669
reidspreiter <reidspreiter@gmail.com>
2024-11-17 12:48:09 -0600

    src/apis/supabase: convert to builder pattern
    The supabase pattern only provides limited functionality, but it
    will be easier to expand if needed.

--- src/apis/supabase.rs
+++ src/apis/supabase.rs
@@ -1,144 +1,195 @@
-use super::base::{request_builder, APIResult, ContentType, RequestBuilder, RequestMethod};
-use super::deezer::Track;
-use super::dictionary::Word;
+use super::base::{request_builder, ContentType, RequestBuilder, RequestMethod};
 use crate::Result;
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Serialize};
 use std::env;
 
-fn supabase_request_builder(method: RequestMethod, url: &str) -> Result<RequestBuilder> {
+fn supabase_request_builder(method: RequestMethod, url: &str, key: &str) -> Result<RequestBuilder> {
     let mut builder = request_builder(method, url);
-    let key = env::var("SUPABASE_RLS_KEY")?;
     builder = builder
-        .header("apikey", &key)
-        .bearer(&key)
+        .header("apikey", key)
+        .bearer(key)
         .content_type(ContentType::JSON)
         .header("Prefer", "return=representation");
     Ok(builder)
 }
 
-fn get_supabase_url() -> Result<String> {
-    Ok(env::var("SUPABASE_URL")?)
+pub enum OrderDirection {
+    DESC,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct MashupAssetsInsert {
-    pub track1: TrackAsset,
-    pub track2: TrackAsset,
-
-    #[serde(rename = "mashedTrack")]
-    pub mashed_track: MashedTrackAsset,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct MashupAssets {
-    pub id: i8,
-
-    #[serde(rename = "createdAt")]
-    pub created_at: String,
-    pub track1: TrackAsset,
-    pub track2: TrackAsset,
-
-    #[serde(rename = "mashedTrack")]
-    pub mashed_track: MashedTrackAsset,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct TrackAsset {
-    pub id: u64,
-    pub title: String,
-
-    #[serde(rename = "fullTitle")]
-    pub full_title: String,
-    pub artist: String,
-    pub preview: String,
-
-    #[serde(rename = "albumTitle")]
-    pub album_title: String,
-
-    #[serde(rename = "coverUrl")]
-    pub cover_url: String,
-    pub origin: TrackOrigin,
-}
-
-impl TrackAsset {
-    pub fn from_track(track: Track, preview: String, origin: TrackOrigin) -> Self {
-        Self {
-            id: track.id,
-            title: track.title,
-            full_title: track.full_title,
-            artist: track.artist.name,
-            preview,
-            album_title: track.album.title,
-            cover_url: track.album.cover_url,
-            origin,
+impl OrderDirection {
+    fn as_str(&self) -> &'static str {
+        match self {
+            OrderDirection::DESC => "desc",
         }
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct TrackOrigin {
-    pub word: Word,
-
-    #[serde(rename = "totalTracks")]
-    pub total_tracks: u64,
-
-    #[serde(rename = "trackIndex")]
-    pub track_index: u64,
+pub struct SupabaseClient {
+    base_url: String,
+    key: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct MashedTrackAsset {
-    pub title: String,
-    pub artist: String,
-
-    #[serde(rename = "albumTitle")]
-    pub album_title: String,
-}
-
-// TODO: convert supabase calls into a builder pattern and potentially expand to a library replicating the javascript one
-
-pub async fn insert<T>(table_name: &str, row: &T) -> Result<APIResult<Vec<T>>>
-where
-    T: DeserializeOwned + Serialize,
-{
-    let url = format!("{}/{}", get_supabase_url()?, table_name);
-    supabase_request_builder(RequestMethod::POST, &url)?
-        .json(row)
-        .request_model::<Vec<T>>()
-        .await
-}
-
-pub async fn select<T>(
-    table_name: &str,
-    columns: Option<&str>,
-    order_by: Option<&str>,
-    limit: Option<&u64>,
-) -> Result<APIResult<Vec<T>>>
-where
-    T: DeserializeOwned,
-{
-    let _columns = columns.unwrap_or("*");
-    let mut url = format!("{}/{}?select={}", get_supabase_url()?, table_name, _columns);
-    if let Some(o) = order_by {
-        url.push_str(&format!("&order={o}"));
+impl SupabaseClient {
+    pub fn new() -> Result<Self> {
+        let base_url = env::var("SUPABASE_URL")?;
+        let key = env::var("SUPABASE_RLS_KEY")?;
+        Ok(Self { base_url, key })
     }
-    if let Some(l) = limit {
-        url.push_str(&format!("&limit={l}"));
+
+    pub fn from(self, table_name: &str) -> SupabaseTable {
+        SupabaseTable {
+            client: self,
+            table_name: table_name.to_string(),
+        }
     }
-    supabase_request_builder(RequestMethod::GET, &url)?
-        .request_model::<Vec<T>>()
-        .await
+}
+pub struct SupabaseTable {
+    client: SupabaseClient,
+    table_name: String,
 }
 
-pub async fn delete<T>(table_name: &str, criteria: Option<&str>) -> Result<APIResult<Vec<T>>>
-where
-    T: DeserializeOwned,
-{
-    let mut url = format!("{}/{}", get_supabase_url()?, table_name);
-    if let Some(c) = criteria {
-        url.push_str(&format!("?{c}"));
+impl SupabaseTable {
+    fn table_url(&self) -> String {
+        format!("{}/{}", self.client.base_url, self.table_name)
     }
-    supabase_request_builder(RequestMethod::DELETE, &url)?
-        .request_model::<Vec<T>>()
-        .await
+
+    pub fn select(self) -> SelectBuilder {
+        SelectBuilder::new(self)
+    }
+
+    pub fn insert<J>(self, row: J) -> InsertBuilder<J>
+    where
+        J: Serialize + DeserializeOwned,
+    {
+        let rows = vec![row];
+        InsertBuilder::new(self, rows)
+    }
+
+    pub fn delete(self) -> DeleteBuilder {
+        DeleteBuilder::new(self)
+    }
+}
+
+pub struct SelectBuilder {
+    table: SupabaseTable,
+    columns: Option<&'static str>,
+    limit: Option<u64>,
+    order_column: Option<&'static str>,
+    order_direction: Option<OrderDirection>,
+}
+
+impl SelectBuilder {
+    pub fn new(table: SupabaseTable) -> Self {
+        Self {
+            table,
+            columns: None,
+            limit: None,
+            order_column: None,
+            order_direction: None,
+        }
+    }
+
+    pub fn order(mut self, column: &'static str, direction: OrderDirection) -> Self {
+        self.order_column = Some(column);
+        self.order_direction = Some(direction);
+        self
+    }
+
+    pub fn limit(mut self, limit: u64) -> Self {
+        self.limit = Some(limit);
+        self
+    }
+
+    pub async fn request<T>(&self) -> Result<Vec<T>>
+    where
+        T: DeserializeOwned,
+    {
+        let columns = self.columns.unwrap_or("*");
+        let mut url = format!("{}?select={}", self.table.table_url(), columns);
+
+        if let (Some(column), Some(direction)) = (self.order_column, &self.order_direction) {
+            url.push_str(&format!("&order={}.{}", column, direction.as_str()));
+        }
+        if let Some(l) = self.limit {
+            url.push_str(&format!("&limit={l}"));
+        }
+        Ok(
+            supabase_request_builder(RequestMethod::GET, &url, &self.table.client.key)?
+                .request_model::<Vec<T>>()
+                .await?
+                .response,
+        )
+    }
+}
+
+pub struct InsertBuilder<J>
+where
+    J: Serialize + DeserializeOwned,
+{
+    table: SupabaseTable,
+    rows: Vec<J>,
+}
+
+impl<J> InsertBuilder<J>
+where
+    J: Serialize + DeserializeOwned,
+{
+    pub fn new(table: SupabaseTable, rows: Vec<J>) -> Self {
+        Self { table, rows }
+    }
+
+    pub async fn request(&self) -> Result<Vec<J>> {
+        let url = self.table.table_url();
+        Ok(
+            supabase_request_builder(RequestMethod::POST, &url, &self.table.client.key)?
+                .json(&self.rows)
+                .request_model::<Vec<J>>()
+                .await?
+                .response,
+        )
+    }
+}
+
+pub struct DeleteBuilder {
+    table: SupabaseTable,
+    notin_column: Option<&'static str>,
+    notin_values: Option<Vec<String>>,
+}
+
+impl DeleteBuilder {
+    pub fn new(table: SupabaseTable) -> Self {
+        Self {
+            table,
+            notin_column: None,
+            notin_values: None,
+        }
+    }
+
+    pub fn notin<I>(mut self, column: &'static str, values: I) -> Self
+    where
+        I: IntoIterator<Item = String>,
+    {
+        let notin_values: Vec<String> = values.into_iter().collect();
+        self.notin_values = Some(notin_values);
+        self.notin_column = Some(column);
+        self
+    }
+
+    pub async fn request<T>(&self) -> Result<Vec<T>>
+    where
+        T: DeserializeOwned,
+    {
+        let mut url = self.table.table_url();
+
+        if let (Some(column), Some(values)) = (self.notin_column, &self.notin_values) {
+            url.push_str(&format!("?{}=not.in.({})", column, values.join(",")));
+        }
+        Ok(
+            supabase_request_builder(RequestMethod::DELETE, &url, &self.table.client.key)?
+                .request_model::<Vec<T>>()
+                .await?
+                .response,
+        )
+    }
 }
