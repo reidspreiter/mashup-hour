d15a2f811b128b914e5111ada50fc6fd07072350
reidspreiter <reidspreiter@gmail.com>
2024-11-17 11:26:27 -0600

    src/assets/manager: chunk mashup assets into cache

--- src/assets/manager.rs
+++ src/assets/manager.rs
@@ -1,12 +1,14 @@
 use super::track::{build_track_asset, mash_track_assets};
 use crate::{
     apis::supabase::{self as sb, MashupAssets},
-    Result,
+    Error, Result,
 };
 use actix_web::web::Data;
-use log::info;
-use redis::{aio::MultiplexedConnection, AsyncCommands, Client};
-use std::sync::Arc;
+use log::{info, warn};
+use redis::{aio::MultiplexedConnection, AsyncCommands, Client, FromRedisValue};
+use std::{cmp, sync::Arc};
+
+const TRACK_LIMIT: u8 = 3;
 
 async fn insert_new_asset_row() -> Result<()> {
     let track1 = build_track_asset().await?;
@@ -42,20 +44,44 @@ async fn select_assets_from_database() -> Result<Vec<sb::MashupAssets>> {
     let assets = sb::select::<sb::MashupAssets>(
         "mashup_assets",
         Some("*"),
-        Some("created_at.desc"),
-        Some(&3),
+        Some("createdAt.desc"),
+        Some(&(TRACK_LIMIT as u64)),
     )
     .await?;
     info!("Selected: {:?}", assets.response);
     Ok(assets.response)
 }
 
+fn chunk_string(s: &str, chunk_size: usize) -> Vec<String> {
+    let mut chunks = Vec::new();
+    let mut start = 0;
+
+    while start < s.len() {
+        let end = cmp::min(start + chunk_size, s.len());
+        let chunk = &s[start..end];
+        chunks.push(chunk.to_string());
+        start = end;
+    }
+    chunks
+}
+
 async fn refresh_assets_cache(
     assets: &Vec<sb::MashupAssets>,
-    connection: &mut MultiplexedConnection,
+    conn: &mut MultiplexedConnection,
 ) -> Result<()> {
+    let chunk_size: usize = 786_423;
+    let expiration = 14_000;
     let json = serde_json::to_string(&assets)?;
-    let _: () = connection.set("mashup_assets", &json).await?;
+    let chunks = chunk_string(&json, chunk_size);
+    let _: () = conn
+        .set_ex("total_chunks", chunks.len() as u8, expiration)
+        .await?;
+
+    for (i, chunk) in chunks.iter().enumerate() {
+        let key = format!("chunk{}", i + 1);
+        let _: () = conn.set_ex(key, chunk, expiration).await?;
+    }
+
     info!("Refreshed assets in cache");
     Ok(())
 }
@@ -73,19 +99,43 @@ pub async fn refresh_assets(client: &Data<Arc<Client>>) -> Result<()> {
     Ok(())
 }
 
-pub async fn retrieve_assets(client: &Data<Arc<Client>>) -> Result<Vec<MashupAssets>> {
-    let mut conn = client.get_multiplexed_tokio_connection().await?;
-    let cached_assets: Option<String> = conn.get("mashup_assets").await?;
-    match cached_assets {
-        Some(assets) => {
-            info!("Retrieved assets from cache");
-            Ok(serde_json::from_str::<Vec<MashupAssets>>(&assets)?)
-        }
+async fn get_cached_value<T>(conn: &mut MultiplexedConnection, key: &str) -> Result<T>
+where
+    T: FromRedisValue,
+{
+    match conn.get::<_, Option<T>>(key).await? {
+        Some(val) => Ok(val),
         None => {
-            info!("Cache miss");
-            let assets = select_assets_from_database().await?;
-            refresh_assets_cache(&assets, &mut conn).await?;
-            Ok(assets)
+            warn!("Cache miss for key: {}", key);
+            let db_assets = select_assets_from_database().await?;
+            refresh_assets_cache(&db_assets, conn).await?;
+
+            // The cache was refreshed, so this should result in a value
+            if let Some(val) = conn.get::<_, Option<T>>(&key).await? {
+                Ok(val)
+            } else {
+                Err(Error::CriticalError(
+                    "Unable to retrieve assets from cache".into(),
+                ))
+            }
         }
     }
+}
+
+pub async fn retrieve_assets(client: &Data<Arc<Client>>) -> Result<Vec<MashupAssets>> {
+    let mut conn = client.get_multiplexed_tokio_connection().await?;
+    let total_chunks = get_cached_value::<u8>(&mut conn, "total_chunks").await?;
+
+    let mut chunks: Vec<String> = Vec::new();
+
+    for i in 1..=total_chunks {
+        let key = format!("chunk{i}");
+        let chunk = get_cached_value::<String>(&mut conn, &key).await?;
+        chunks.push(chunk);
+    }
+
+    let combined_chunks = chunks.join("");
+    let assets: Vec<MashupAssets> = serde_json::from_str(&combined_chunks)?;
+    info!("Retrieved assets from cache");
+    Ok(assets)
 }
