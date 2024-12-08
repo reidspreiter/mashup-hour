3fbb4788c482041f9866081786568c0c0ecee07c
reidspreiter <reidspreiter@gmail.com>
2024-12-08 14:11:00 -0600

    f/src/componets/player/player: add position control and setters/getters
    The tracks position can now be tracked fairly accurately, but there are
    still improvements to be made. Player should be passed a position
    callback to be called at regularly scheduled intervals, and also
    irregularly when the player stops and starts and loops and changes
    direction etc.
    
    Additionally, convert the enabler and changer functions to setters and
    getters as they are more readable.

--- mashup-hour-frontend/src/components/player/player.ts
+++ mashup-hour-frontend/src/components/player/player.ts
@@ -1,14 +1,28 @@
 import * as Tone from "tone"
 
+//keep track of last elapsed time before the playback rate change until loop restarts
+
 export class Player {
+    readonly name: string;
+
     private previewUrl: string;
     private blob: Blob;
     private player: Tone.Player;
-    private playbackSpeedPitcher: Tone.PitchShift;
-    private playbackSpeedPitcherEnabled: boolean = false;
 
-    constructor(preview: string, setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>) {
-        console.log("Constructing")
+    private playbackSpeedPitcher: Tone.PitchShift;
+    private consistentPitch: boolean = false;
+
+    private _startBound: number = 0;
+    private _endBound: number = 0;
+
+    private _position: number = 0;
+    private timeReference: number = 0;
+
+    public restartOnPause: boolean = false;
+
+    constructor(name: string, preview: string, setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>) {
+        this.name = name;
+
         const binaryPreview = atob(preview);
         const arrayBuffer = new ArrayBuffer(binaryPreview.length);
         const uint8Array = new Uint8Array(arrayBuffer);
@@ -22,55 +36,64 @@ export class Player {
             pitch: 0,
             windowSize: 0.1,
         }).toDestination();
+
         this.player = new Tone.Player(this.previewUrl).connect(this.playbackSpeedPitcher);
-        this.player.onstop = () => setIsPlaying(false);
+        this.player.onstop = () => {
+            setIsPlaying(false);
+            this.refreshPosition(true);
+        }
+
+        this.player.buffer.onload = () => {
+            this._endBound = this.player.buffer.duration;
+        }
     }
 
     public togglePlayer = (isEnabled: boolean) => {
-        console.log(`toggling player ${this.previewUrl}`)
         if (isEnabled) {
             Tone.start();
-            // this.player.start(undefined, this.player.loopStart, Tone.Time(this.player.loopEnd).toSeconds() - Tone.Time(this.player.loopStart).toSeconds());
-            this.player.start(undefined, this.player.loopStart);
+            if (this.restartOnPause) {
+                this._position = (this.reverse ? this._endBound : this._startBound) * 1000;
+            } else {
+                this._position = this._position === 0 && this.reverse ? this._endBound * 1000 : this._position;
+            }
+            this.player.start(undefined, this.reverse ? this.duration - this._position / 1000 : this._position / 1000);
+            this.timeReference = performance.now();
         } else {
             this.player.stop();
         }
     }
 
-    public toggleLoop = (isEnabled: boolean) => {
-        console.log("toggle loop is called")
-        this.player.loop = isEnabled;
+    set loop(loop: boolean) {
+        this.player.loop = loop;
     }
 
-    public toggleReverse = (isEnabled: boolean) => {
-        this.player.reverse = isEnabled;
+    get reverse(): boolean {
+        return this.player.reverse
     }
 
-    public adjustPlaybackSpeed = (newValue: number) => {
-        if (this.playbackSpeedPitcherEnabled) {
-            this.playbackSpeedPitcher.pitch = -12 * Math.log2(newValue);
-            console.log(this.playbackSpeedPitcher.pitch);
-            console.log(newValue);
+    set reverse(reverse: boolean) {
+        this.player.reverse = reverse;
+    }
+
+    set playbackRate(rate: number) {
+        if (this.consistentPitch) {
+            this.playbackSpeedPitcher.pitch = -12 * Math.log2(rate);
         }
-        this.player.playbackRate = newValue;
-
+        this.player.playbackRate = rate;
+        this.refreshPosition();
     }
 
-    public adjustVolume = (newValue: number) => {
-        this.player.volume.value = newValue;
+    set volume(volume: number) {
+        this.player.volume.value = volume;
     }
 
-    public toggleMute = (isEnabled: boolean) => {
-        if (isEnabled) {
-            this.player.mute = true;
-        } else {
-            this.player.mute = false;
-        }
+    set mute(mute: boolean) {
+        this.player.mute = mute;
     }
 
-    public togglePitchConsistentPlayback = (isEnabled: boolean) => {
-        this.playbackSpeedPitcherEnabled = isEnabled;
-        if (isEnabled) {
+    set pitchConsistentPlayback(pitchConsistentPlayback: boolean) {
+        this.consistentPitch = pitchConsistentPlayback;
+        if (pitchConsistentPlayback) {
             // this.player.connect(this.playbackSpeedPitcher);
             this.playbackSpeedPitcher.pitch = -12 * Math.log2(this.player.playbackRate);
         } else {
@@ -79,23 +102,46 @@ export class Player {
         }
     }
 
-    public isStarted = (): boolean => {
-        return this.player.state === "started"
+    get isStarted(): boolean {
+        return this.player.state === "started";
     }
 
-    public totalDuration = (): number => {
+    get duration(): number {
         return this.player.buffer.duration;
     }
 
-    public setStart = (start: number) => {
+    set startBound(start: number) {
         this.player.loopStart = start;
+        this._startBound = start;
     }
 
-    public setEnd = (end: number) => {
+    set endBound(end: number) {
         this.player.loopEnd = end;
+        this._endBound = end;
     }
 
     public setBounds = (start: number, end: number) => {
         this.player.setLoopPoints(start, end);
+        this._startBound = start;
+        this._endBound = end;
+    }
+
+    private refreshPosition(forceRefresh: boolean = false) {
+        if (this.isStarted || forceRefresh) {
+            const now = performance.now();
+            const op = this.reverse ? -1 : 1
+            this._position = (this._position + (now - this.timeReference) * this.player.playbackRate * op) % (this.duration * 1000);
+            // TODO: call the position callback when loops
+            this.timeReference = now;
+        } else if (!this.isStarted && this.restartOnPause) {
+            this._position = this.reverse ? this._endBound : this._startBound;
+        }
+    }
+
+    get position(): number {
+        // TODO: assign this to a callback for better control
+        this.refreshPosition()
+        return this._position / 1000;
     }
 }
+
