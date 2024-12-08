a1b6c337889d2b43c45650b6eba94a292077b9ab
reidspreiter <reidspreiter@gmail.com>
2024-12-08 14:14:43 -0600

    f/src/components/track/TrackPlayer: use player setters and getters

--- mashup-hour-frontend/src/components/track/TrackPlayer.tsx
+++ mashup-hour-frontend/src/components/track/TrackPlayer.tsx
@@ -9,16 +9,15 @@ interface TrackPlayerProps {
 }
 
 const TrackPlayer: React.FC<TrackPlayerProps> = ({ player, isPlaying, setIsPlaying }) => {
-    console.log("Player rerendering")
     return (
         <section>
             <div className="main-column-section" style={{ marginBottom: "10px" }}><PlayBar player={player} /></div>
             <div className="main-column-section">
-                <Casing sub1={<SimpleSwitch name="preserve pitch" onClick={player.togglePitchConsistentPlayback} />}><Knob name="speed" min={0.1} max={3} mid={1} onChange={player.adjustPlaybackSpeed} icon={IoSpeedometer} /></Casing>
-                <Casing sub2={<SimpleSwitch name="lock playbar bounds" onClick={() => { }} />}><Switch name="reverse" icon={IoArrowBackSharp} onClick={player.toggleReverse} /></Casing>
+                <Casing sub1={<SimpleSwitch name="preserve pitch" onClick={(isEnabled: boolean) => player.pitchConsistentPlayback = isEnabled} />}><Knob name="speed" min={0.1} max={3} mid={1} onChange={(newValue: number) => player.playbackRate = newValue} icon={IoSpeedometer} /></Casing>
+                <Casing sub2={<SimpleSwitch name="lock playbar bounds" onClick={() => { }} />}><Switch name="reverse" icon={IoArrowBackSharp} onClick={(isEnabled: boolean) => player.reverse = isEnabled} /></Casing>
                 <Casing sub1={<SimpleButton name="restart" onClick={() => { }} />} sub2={<SimpleSwitch name="restart on pause" onClick={() => { }} />}><PlayButton onClick={player.togglePlayer} isPlaying={isPlaying} setIsPlaying={setIsPlaying} /></Casing>
-                <Casing sub1={<SimpleButton name="randomize playbar bounds" onClick={() => { }} />}><Switch name="loop" icon={IoRepeatSharp} onClick={player.toggleLoop} /></Casing>
-                <Casing sub1={<SimpleSwitch name="mute" onClick={player.toggleMute} enabledName="unmute" />} sub2={<PressAndHold name="mute on hold" onClick={player.toggleMute} />}><Knob name="volume" min={-40} mid={-4} max={20} onChange={player.adjustVolume} icon={IoVolumeMedium} /></Casing>
+                <Casing sub1={<SimpleButton name="randomize playbar bounds" onClick={() => { }} />}><Switch name="loop" icon={IoRepeatSharp} onClick={(isEnabled: boolean) => player.loop = isEnabled} /></Casing>
+                <Casing sub1={<SimpleSwitch name="mute" onClick={(isEnabled: boolean) => player.mute = isEnabled} enabledName="unmute" />} sub2={<PressAndHold name="mute on hold" onClick={(isEnabled: boolean) => player.mute = isEnabled} />}><Knob name="volume" min={-40} mid={-4} max={20} onChange={(newValue: number) => player.volume = newValue} icon={IoVolumeMedium} /></Casing>
 
             </div>
         </section >
