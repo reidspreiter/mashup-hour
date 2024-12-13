f35f091a02b18bd8e9a48f5b276b1d6bebd65807
reidspreiter <reidspreiter@gmail.com>
2024-12-12 22:12:19 -0600

    f/src/components/controllers/PlayButton: assign player's onStop method

--- mashup-hour-frontend/src/components/controllers/PlayButton.tsx
+++ mashup-hour-frontend/src/components/controllers/PlayButton.tsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from "react";
 import { IoPauseCircleSharp, IoPlayCircleSharp } from "react-icons/io5";
+import { Player } from "../player";
 import Tooltip from "./Tooltip";
 import "./styles/controllers.css";
 import { EnablerFunc } from "./types";
 
 interface PlayButtonProps {
-  isPlaying: boolean;
-  setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+  player?: Player;
   onClick: EnablerFunc;
 }
 
-const PlayButton: React.FC<PlayButtonProps> = ({ onClick, isPlaying, setIsPlaying }) => {
+const PlayButton: React.FC<PlayButtonProps> = ({ onClick, player }) => {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (player !== undefined) {
+      player.onStop = () => setIsPlaying(false);
+    }
+  }, [player]);
+
   return (
     <Tooltip text={isPlaying ? "pause" : "play"}>
       <button
